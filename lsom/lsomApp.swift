bd40caa6e1b4075c768e80503f685308df2bc855
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2025-12-26 23:56:54 +0200

    Initial Commit

---
+++ lsom/lsomApp.swift
@@ -0,0 +1,17 @@
+//
+//  lsomApp.swift
+//  lsom
+//
+//  Created by Mike Zamagias on 26/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct lsomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
